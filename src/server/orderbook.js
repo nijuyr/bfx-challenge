036e1f19cd4f796aece53b2504f895ab6fe9adb5
Eugenee <eugenee@copper.local>
2023-01-21 23:35:12 +1100

    sq:server orderbook

--- src/server/orderbook.js
+++ src/server/orderbook.js
@@ -1,21 +1,21 @@
 class Orderbook {
     constructor() {
-        this.orders = new Map();
+        this.orders = new Map()
     }
 
     getBuyOrders() {
-        return Array.from(this.orders.values()).filter(order => order.type === 'buy');
+        return Array.from(this.orders.values()).filter(order => order.type === 'buy')
     }
     
     getSellOrders() {
-        return Array.from(this.orders.values()).filter(order => order.type === 'sell');
+        return Array.from(this.orders.values()).filter(order => order.type === 'sell')
     }
 
     //** takes an order with buy/sell order type, and store in Map */
     addOrder(order) {
-        this.orders.set(order.id, order);
-        console.log(`Order added: ${order.id}`);
-        this.matchOrders();
+        this.orders.set(order.id, order)
+        console.log(`Order added: ${order.id}`)
+        this.matchOrders()
     }
 
     //** matches orders stored in Map based on the order price. if matches then process the order and reduce */
@@ -23,21 +23,21 @@ class Orderbook {
         const buyOrders = this.getBuyOrders()
         const sellOrders = this.getSellOrders()
         const matchedOrders = buyOrders.reduce((matched, buyOrder) => {
-            const sellOrder = sellOrders.find(order => order.price <= buyOrder.price);
+            const sellOrder = sellOrders.find(order => order.price <= buyOrder.price)
             if (sellOrder) {
-                matched.push({ buyOrder, sellOrder });
-                sellOrders.splice(sellOrders.indexOf(sellOrder), 1);
+                matched.push({ buyOrder, sellOrder })
+                sellOrders.splice(sellOrders.indexOf(sellOrder), 1)
             }
-            return matched;
-        }, []);
+            return matched
+        }, [])
 
         matchedOrders.forEach(({buyOrder, sellOrder}) => {
             //! execute trade
-            this.orders.delete(buyOrder.id);
-            this.orders.delete(sellOrder.id);
-            console.log(`Orders matched: ${buyOrder.id}, ${sellOrder.id} `);
+            this.orders.delete(buyOrder.id)
+            this.orders.delete(sellOrder.id)
+            console.log(`Orders matched: ${buyOrder.id}, ${sellOrder.id} `)
             console.log(`Orders left: ${this.orders.size}`)
-         });
+        })
     }
 }
 
